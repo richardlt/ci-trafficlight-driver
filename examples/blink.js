f0b7751f29e48d89a87cb55098a110b17a6c2a34
richardlt <richard.le.terrier@gmail.com>
2016-07-13 14:00:09 +0200

    feat: Create example file, add npm ignore

--- examples/blink.js
+++ examples/blink.js
@@ -1,7 +1,25 @@
 var CiTrafficlightDriver = require("./../src/ci-trafficlight-driver.js");
 
-var driver = CiTrafficlightDriver.init(500);
+var driver = CiTrafficlightDriver.init(50);
 
-driver.setColor("red");
+var time = 500;
+
+var sequence = function() {
+  driver.setColor("red");
+  setTimeout(function() {
+    driver.setColor("orange");
+    setTimeout(function() {
+      driver.setColor("green");
+      setTimeout(function() {
+        driver.setColor("orange");
+        setTimeout(function() {
+          sequence();
+        }, time);
+      }, time);
+    }, time);
+  }, time);
+}
+
+sequence();
 
 driver.setBlink(true);
